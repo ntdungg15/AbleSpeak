0fc024fbc2a9c9cfc07a7f0ffd78acaac2249a0a
longhoanglekim <longngerr@gmail.com>
2025-04-22 22:52:46 +0700

    feat : api for dialogue and grammar

---
+++ engapp/src/main/java/com/uet/longhoang/engapp/engapp/controllers/DialogueController.java
@@ -0,0 +1,21 @@
+package com.uet.longhoang.engapp.engapp.controllers;
+
+import com.uet.longhoang.engapp.engapp.entities.Dialogue;
+import com.uet.longhoang.engapp.engapp.services.impls.DialogueService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/dialogue")
+public class DialogueController {
+    @Autowired
+    DialogueService dialogueService;
+    @GetMapping("/dialogue")
+    public List<Dialogue> getDialogues() {
+        return dialogueService.getAllDialogues();
+    }
+}
---
+++ engapp/src/main/java/com/uet/longhoang/engapp/engapp/entities/Dialogue.java
@@ -0,0 +1,23 @@
+package com.uet.longhoang.engapp.engapp.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.annotation.Id;
+
+@Document(collection = "dialogues")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Dialogue {
+
+    @Id
+    private String id; // MongoDB sẽ tự động tạo trường _id cho bạn
+
+    private String character;
+    private String text;
+    private String audioLink;
+    private String translation;
+}
