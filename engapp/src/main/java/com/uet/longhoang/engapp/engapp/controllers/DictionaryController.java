f5b2cde0aad31b43a756c40c51b1f9bfb3d43a56
longhoanglekim <longngerr@gmail.com>
2025-04-29 00:20:20 +0700

    feat : translate api

--- engapp/src/main/java/com/uet/longhoang/engapp/engapp/controllers/DictionaryController.java
+++ engapp/src/main/java/com/uet/longhoang/engapp/engapp/controllers/DictionaryController.java
@@ -1,9 +1,13 @@
 package com.uet.longhoang.engapp.engapp.controllers;
 
-import com.uet.longhoang.engapp.engapp.entities.DictionaryEntry;
+import com.uet.longhoang.engapp.engapp.dtos.TranslationRequest;
 import com.uet.longhoang.engapp.engapp.services.impls.DictionaryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/dictionary")
@@ -13,8 +17,13 @@ public class DictionaryController {
     @Autowired
     private DictionaryService dictionaryService;
 
-    @GetMapping("/word/{word}")
-    public DictionaryEntry[] getWordDefinition(@PathVariable String word) {
-        return dictionaryService.getDefinition(word);
+    @GetMapping("/translate")
+    public ResponseEntity<?> getWordDefinition(@RequestBody TranslationRequest request) {
+        try {
+            String res =  dictionaryService.translate(request.getFrom(), request.getTo(), request.getText());
+            return new ResponseEntity<>(res, HttpStatus.OK);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
