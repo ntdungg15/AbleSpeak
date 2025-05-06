51bd93c026b48b30144571a82bf756b9e829c15a
longhoanglekim <longngerr@gmail.com>
2025-05-06 14:17:50 +0700

    feat : update user info

--- engapp/src/main/java/com/uet/longhoang/engapp/controllers/UserController.java
+++ engapp/src/main/java/com/uet/longhoang/engapp/controllers/UserController.java
@@ -1,27 +1,40 @@
 package com.uet.longhoang.engapp.engapp.controllers;
 
+import com.uet.longhoang.engapp.engapp.dtos.UpdateUserDTO;
+import com.uet.longhoang.engapp.engapp.dtos.UserInfo;
 import com.uet.longhoang.engapp.engapp.entities.User;
 import com.uet.longhoang.engapp.engapp.services.impls.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.security.core.parameters.P;
+import org.springframework.web.bind.annotation.*;
+@Slf4j
 @RestController
 @RequestMapping("/user")
 public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("userInfo")
+    @GetMapping("/userInfo")
     public ResponseEntity<?> getUserInfo(@RequestParam String username) {
         if (userService.findByUsername(username).isEmpty()) {
             return ResponseEntity.notFound().build();
         }
         User user = userService.findByUsername(username).get();
         return ResponseEntity.ok(userService.getUserInfo(user));
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<?> updateUserInfo(@RequestBody UpdateUserDTO userInfo) {
+        try {
+            userService.updateUser(userInfo);
+            return ResponseEntity.ok(userInfo);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
     }
 
 }
--- engapp/src/main/java/com/uet/longhoang/engapp/mappers/UserMapper.java
+++ engapp/src/main/java/com/uet/longhoang/engapp/mappers/UserMapper.java
@@ -2,6 +2,8 @@ package com.uet.longhoang.engapp.engapp.mappers;
 
 import com.uet.longhoang.engapp.engapp.dtos.UserInfo;
 import com.uet.longhoang.engapp.engapp.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class UserMapper {
     public static UserInfo mapUserToUserInfo(User u) {
@@ -9,6 +11,7 @@ public class UserMapper {
         userInfo.setEmail(u.getEmail());
         userInfo.setName(u.getName());
         userInfo.setPhone(u.getPhone());
+
         return userInfo;
 
     }
