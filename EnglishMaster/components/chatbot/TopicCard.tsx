31b5bb10ed3ceec54b3b96e71aa010415b34d3f3
ThanhHigh <ducthanhtre2004@gmail.com>
2025-05-07 18:52:09 +0700

    fix chatapp layout

---
+++ EnglishMaster/components/chatbot/TopicCard.tsx
@@ -0,0 +1,15 @@
+
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const TopicCard = () => {
+  return (
+    <View>
+      <Text>TopicCard</Text>
+    </View>
+  )
+}
+
+export default TopicCard
+
+const styles = StyleSheet.create({})
